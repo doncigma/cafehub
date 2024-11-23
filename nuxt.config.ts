933afe540207470d203574abccff43b1678b97f6
Elijah Johnson <astroboy772@gmail.com>
2024-11-22 18:01:25 -0800

    pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/tailwindcss'],
-  runtimeConfig: {
-    public: {
-      transLogoSrc: 'logos/CafeHub-logo-trans.png',
-      noTextLogoSrc: 'logos/CafeHub-no-text.png'
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: ['@nuxt/ui', '@nuxt/image', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
+    runtimeConfig: {
+        public: {
+            transLogoSrc: 'logos/CafeHub-logo-trans.png',
+            noTextLogoSrc: 'logos/CafeHub-no-text.png'
+        },
     },
-  },
 })
