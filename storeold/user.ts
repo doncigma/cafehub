24ada5b718653eb1eedabe6b77a20117f70fe298
Elijah Johnson <astroboy772@gmail.com>
2024-11-26 19:24:55 -0800

    cm

--- storeold/user.ts
+++ storeold/user.ts
@@ -1,20 +1,20 @@
-export const useUserStore = defineStore('user', {
-    state: () => ({
-        email: '',
-        username: '',
-        isLoggedIn: false,
-    }),
-    actions: {
-        login(email: string, username: string) {
-            this.email = email;
-            this.username = username;
-            this.isLoggedIn = true;
-        },
-        logout() {
-            this.email = '';
-            this.username = '',
-            this.isLoggedIn = false;
-        },
-    },
-});
+// export const useUserStore = defineStore('user', {
+//     state: () => ({
+//         email: '',
+//         username: '',
+//         isLoggedIn: false,
+//     }),
+//     actions: {
+//         login(email: string, username: string) {
+//             this.email = email;
+//             this.username = username;
+//             this.isLoggedIn = true;
+//         },
+//         logout() {
+//             this.email = '';
+//             this.username = '',
+//             this.isLoggedIn = false;
+//         },
+//     },
+// });
 
